93904fd321db81876d6dd5a9161fd0390a1ed6f1
Sacha <urswiit@gmail.com>
2020-12-18 12:03:13 +0100

    add info routes

---
+++ resources/views/info.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/info.css')}}">
+    <title>Document</title>
+</head>
+<body>
+     <div class="container">
+         <p>
+           <strong>Infos </strong><br>
+           <br>
+           Les infos vont mettre en avant les les différents types 
+           d’informations qui vous aidera au cas où !<br>
+            <br>
+           HANDICAP, MALADIES, ALLERGIES<br>
+           <br>
+           Vous avez une question liée à des réservations/visites 
+           pour les personnes en situation d'handicap ou qui souffrent 
+           d'allergies alimentaires. Utilisez notre formulaire dédié en 
+           cliquant ici .<br>
+
+           Pour toute autre question, merci de nous contacter au<br>
+           <br>
+           06 12 34 56 78*<br>
+           *Service 0,15 €/min + prix appel<br>
+           <br>
+           <br>
+            <strong>OBJET PERDU</strong><br>
+            <br>
+            Si vous avez égaré un objet personnel lors de votre visite 
+            dans nos parcs, vous pouvez contacter notre service<br>
+            Objets Trouvés en cliquant ici :<br>
+            <br>
+            <strong>gamezonecontactservice@gmail.com</strong><br>
+            <br>
+            <br>
+            <strong>DEMANDE DE FACTURE</strong><br>
+            <br>
+            Si vous souhaitez recevoir une facture acquittée suite à un 
+            séjour, une visite ou un achat à Game Zone, nous vous 
+            invitons à nous contacter à l’adresse suivante :<br>
+            <br>
+            <strong>gamezonecontactservice@gmail.com</strong><br>
+            <br>
+            en communiquant vos nom, prénom, adresse postale et votre 
+            numéro de réservation (si votre demande est liée à un séjour) 
+            et/ou le ticket qui vous a été remis lors de la transaction.<br>
+            <br>
+            <br>
+            <strong>SERVICE APRÈS VENTE BOUTIQUES</strong><br>
+            <br>
+            En cas de réclamation n’ayant pas donné lieu à une réponse 
+            satisfaisante dans un délai de 45 jours, vous avez la 
+            possibilité de saisir le Médiateur du Tourisme et du Voyage, 
+            dont les coordonnées et modalités de saisine peuvent être 
+            obtenues en consultant son site Internet :<br>
+             <a href="http://www.mtv.travel">http://www.mtv.travel</a><br>
+             <br>
+             <br>
+             <strong>RÉCLAMATION APRÈS SÉJOUR</strong><br>
+             <br>
+             Votre visite n’est pas terminée et vous souhaitez nous faire 
+             d’une déception ? Pour toute demande ou commentaire liés à votre 
+             parc, nous vous invitons à vous présenter directement à la 
+             réception de ce dernier.<br>
+             <br>
+             Vous avez déjà effectué votre visite et souhaitez nous faire 
+             part d'une réclamation? Pour en faciliter le traitement, 
+             veuillez utiliser notre formulaire de contact dédié en cliquant 
+             ici ou envoyer un e-mail à l’adresse suivante :<br> 
+             <strong>gamezonecontactservice@gmail.com</strong><br>
+             <br>
+             En cas de réclamation n’ayant pas donné lieu à une réponse 
+             satisfaisante dans un délai de 45 jours, vous avez la 
+             possibilité de saisir le Médiateur du Tourisme et du Voyage, 
+             dont les coordonnées et modalités de saisine peuvent être 
+             obtenues en consultant son site Internet :<br> 
+             <a href="http://www.mtv.travel">http://www.mtv.travel</a> . 
+             Pour le règlement des litiges relatifs à des transactions en 
+             ligne, vous pouvez vous rendre sur la plate-forme mise en place 
+             par la Commission européenne à l’adresse suivante :<br>
+             <a href="http://ec.europa.eu/odr/">http://ec.europa.eu/odr/</a><br>
+             <br>
+             <br>
+              <strong>RÈGLES DE RESPECT DE LA VIE PRIVÉE</strong><br>
+              <br>
+              Vous disposez d'un certain nombre de droits, notamment le 
+              droit de modifier, supprimer ou demander l'accès à vos 
+              informations à caractère personnel, ainsi que le droit de 
+              modifier vos préférences marketing (ce qui comprend la 
+              possibilité de retirer votre consentement à tout moment). 
+              Veuillez consulter nos Règles de respect de la vie privée 
+              pour en savoir plus sur la façon dont nous gérons vos 
+              informations à caractère personnel, ou contactez 
+              <strong>gamezonecontactservice@gmail.com</strong><br>
+              <br>
+              <br>
+              <strong>DEMANDE DE LOTS ET OBJETS PUBLICITAIRES</strong><br>
+              <br>
+              Vous souhaitez recevoir des lots et/ou objets publicitaires. 
+              DGame Zone ne dispose pas d'objets publicitaires et de lots 
+              à envoyer à la clientèle.<br>
+              <br>
+              <br>
+              <strong>DEMANDE DE LOTS ET OBJETS PUBLICITAIRES</strong><br>
+              <br>
+              Vous souhaitez organiser une visite de groupe à Game Zone ? 
+              Vous souhaitez modifier un dossier en cours, nous faire parvenir 
+              vos commentaires ou encore poser des questions? Cliquez ici .<br>
+              <br>
+              Pour toute demande concernant le tourisme d’affaires , 
+              un site dédié est mis à votre disposition. Cliquez ici .<br>
+              <br>
+              <br>
+              <strong>RECRUTEMENT</strong><br>
+              <br>
+              Pour consulter les offres de recrutement de Game Zone ou 
+              déposer votre candidature , une page dédiée est mis à votre 
+              disposition. Cliquez ici.<br>
+         </p>
+     </div>
+</body>
+</html>
