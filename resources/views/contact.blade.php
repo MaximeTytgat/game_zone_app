602462efb72dae7680ee59fb90e0f06e4dbd63c5
Sacha <urswiit@gmail.com>
2020-12-21 19:47:30 +0100

    fix  responsive

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,40 +10,43 @@
 </head>
 
 <body>
-    <form class="col-6 mx-auto">
-        <div class="col-6 mb-3">
-            <label for="exampleInputEmail1" class="form-label">Nom</label>
-            <input type="name" class="form-control" id="nameInput" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text"></div>
-        </div>
-        <div class="col-6 mb-3">
-            <label for="exampleInputPassword1" class="form-label">E-mail</label>
-            <input type="email" class="form-control" id="mailInput">
-        </div>
-        <div class="col-6 mb-3">
-            <label for="exampleInputPassword1" class="form-label">Objet</label>
-            <input type="name" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Message</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
-        </div>
-        <div class="mb-3  form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">J’accepte les <span class="cgu_color"> Conditions de
-                    vente
-                    et conditions générales d’utilisation</span></label>
-        </div>
-        <button type="submit" id="submitContact" class="btn btn-primary">Envoyer</button>
-    </form>
+    <div id="boite" class="blueBox">
+        <form class="col-6 col-10-sm mx-auto">
+            <div class="col-6 mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nom</label>
+                <input type="name" class="form-control" id="nameInput" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text"></div>
+            </div>
+            <div class="col-6 mb-3">
+                <label for="exampleInputPassword1" class="form-label">E-mail</label>
+                <input type="email" class="form-control" id="mailInput">
+            </div>
+            <div class="col-6 mb-3">
+                <label for="exampleInputPassword1" class="form-label">Objet</label>
+                <input type="name" class="form-control" id="exampleInputPassword1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>
+            </div>
+            <div class="mb-3  form-check">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">J’accepte les <a href="{{asset('cgv')}}">Conditions de
+                        vente
+                        et conditions générales d’utilisation</a></label>
+            </div>
+            <button type="submit" id="submitContact" class="btn btn-primary">Envoyer</button>
+
+        </form>
+    </div>
     <div class="col-5 mt-5 mb-4 mx-auto">
         <h2>Ou encore sur nos résaux sociaux</h2>
     </div>
-    <div class="col-8 mx-auto mt-3 d-flex justify-content-around">
-        <img src="{{asset('images/youtubeLogo.png')}}" alt="Logo youtube">
-        <img src="{{asset('images/insta.png')}}" alt="Logo instagram">
-        <img src="{{asset('images/twitterLogo.png')}}" alt="Logo twitter">
-        <img src="{{asset('images/facebook.png')}}" alt="Logo facebook">
+    <div class="col-8 mx-auto mt-3 d-flex wrap justify-content-around">
+        <img class="m-2" src="{{asset('images/youtubeLogo.png')}}" alt="Logo youtube">
+        <img class="m-2" src="{{asset('images/insta.png')}}" alt="Logo instagram">
+        <img class="m-2" src="{{asset('images/twitterLogo.png')}}" alt="Logo twitter">
+        <img class="m-2" src="{{asset('images/facebook.png')}}" alt="Logo facebook">
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
