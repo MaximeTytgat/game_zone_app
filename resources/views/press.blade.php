e5ea06afbc28fb16f749a8b1218d5ceb01c50f27
Sacha <urswiit@gmail.com>
2020-12-21 23:11:30 +0100

    add recruitment

---
+++ resources/views/press.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/press.css')}}">
+    <title>Presse</title>
+</head>
+
+<body>
+    <div class="titles">
+        <h2>Actualitées a la une </h2>
+        <p>Retrouvez toutes les actualitées en un clic !</p>
+    </div>
+    <div class="container">
+        <h3>Les magazines</h3>
+        <div>
+            <img src="{{asset('images/magazine_1.png')}}" alt="Image magazine 1">
+            <img src="{{asset('images/magazine_2.png')}}" alt="Image magazine 2">
+            <img src="{{asset('images/magazine_3.png')}}" alt="Image magazine 3">
+            <img src="{{asset('images/magazine_4.png')}}" alt="Image magazine 4">
+        </div>
+    </div>
+    <div class="container-2">
+        <h3>Les posts</h3>
+        <div>
+            <img src="{{asset('images/magazine_1.png')}}" alt="Image magazine 1">
+            <img src="{{asset('images/magazine_2.png')}}" alt="Image magazine 2">
+            <img src="{{asset('images/magazine_3.png')}}" alt="Image magazine 3">
+            <img src="{{asset('images/magazine_4.png')}}" alt="Image magazine 4">
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/recruitment.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/recruitment.css')}}">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="recrutement">
+        <h3>L'équipe GameZone recrute !</h3>
+        <p>Voulez vous faire partie de l’aventure Game Zone ?
+            Vous êtes vous passioné par les attractions et les jeux vidéos ?
+
+            Rejoignez notre équipe !</p>
+    </div>
+    <div class="blue_background">
+        <p>Nos postes a pourvoir :</p>
+        <ul>
+            <li>- Technicien (H/F)</li>
+            <li>- Animateur (H/F)</li>
+            <li>- Vendeur (H/F)</li>
+            <li>- Chef d’animation (H/F)</li>
+            <li>- Chef de projet (H/F)</li>
+        </ul>
+    </div>
+</body>
+
+</html>
+</body>
+
+</html>
