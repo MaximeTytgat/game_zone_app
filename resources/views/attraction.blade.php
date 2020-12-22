e1130f916d60b0a76fb27175c37ae1ad05ec98ad
brian <brian.mtr@outlook.fr>
2020-12-22 14:30:12 +0100

    change color attraction

--- resources/views/attraction.blade.php
+++ resources/views/attraction.blade.php
@@ -256,6 +256,7 @@
     <div class="backToTop">
         <a href="{{ route('attraction') }}"><img src="{{asset('images/backToTop.png')}}" alt=""></a>
     </div>
+    @include('footer')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 
